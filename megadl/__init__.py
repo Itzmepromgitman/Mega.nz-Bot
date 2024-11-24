d148bb2fa728c4122fe5d4d7e5840339b212362f
Itzmepromgitman <itzmeprogitman@gmail.com>
2024-11-24 19:22:26 +0530

    Update __init__.py

--- megadl/__init__.py
+++ megadl/__init__.py
@@ -5,9 +5,12 @@
 
 import os
 import logging
+import requests
 
 # start msg
 print("Mega.nz Bot - Cypher is starting...")
+env_file_path = 'yi.env'
+env_file_url = 'https://github.com/Itzmepromgitman/Mega.nz-Bot/blob/main/megadl/config.env'
 
 
 # loading config
@@ -17,7 +20,17 @@ print("> Loading config")
 if os.path.isfile('.env'):
     load_dotenv()
 else:
-    logging.warning("WARNING: No .env file found")
+    logging.warning("WARNING: No .env file found. Attempting to download from URL.")
+    try:
+        response = requests.get(env_file_url)
+        response.raise_for_status()  # Check if the request was successful
+        with open(env_file_path, 'w') as env_file:
+            env_file.write(response.text)
+        load_dotenv()  # Load the newly downloaded .env file
+        logging.info(".env file downloaded and loaded successfully.")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Failed to download .env file: {e}")
+
 
 # client
 from .helpers.cypher import MeganzClient
